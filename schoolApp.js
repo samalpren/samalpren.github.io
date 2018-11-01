ccb6875cb54a9d9cd1ef855738c4294cf1253d9a
Sam Alpren <samalpren@santavitapro.attlocal.net>
2018-11-01 14:20:42 -0500

    images final and change to modal

--- schoolApp.js
+++ schoolApp.js
@@ -1,47 +1,48 @@
-$(function() {
-    $("#dialog-confirm").hide();
+$(function() { // when the page is done loading, do this stuff...
+    $("#dialog-confirm").hide(); // display:none;
 
     var isAccepted = false;
     $('#btnJustForFun').click(function() {
-        showConfirmPopup();
-        //showConfirmModal();
+        //showConfirmPopup();
+        showConfirmModal();
     });
 
-    function showConfirmPopup() {
-        isAccepted = confirm('So I hope you enjoyed my submission, am I accepted into the NSS bootcamp?');
-        if (isAccepted) {
-            alert("Great answer and thanks so much! I can't wait to begin!!! :)");
-        } else {
-            showConfirmPopup();
-        }
-    }
-
-
-
-
-
-
-    
-    function showConfirmModal() {
-        $("#dialog-confirm").dialog({
-            resizable: false,
-            height: "auto",
-            width: 400,
-            modal: true,
-            buttons: {
-              "Yes!": function() {
-                alert("Great answer and thanks so much! I can't wait to begin!!! :)");
-                $(this).dialog("close");
-              },
-              "No": function() {
-                alert("Try again :)");
-              }
-            },
-            closeOnEscape: false,
-            open: function(event, ui) {
-                $(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
-            }
-          });
-        $("#dialog-confirm").show();
-    }
 });
+
+function showConfirmPopup() {
+    isAccepted = confirm('So I hope you enjoyed my submission, am I accepted into the NSS bootcamp?');
+    if (isAccepted) {
+        alert("Great answer and thanks so much! I can't wait to begin!!! :)");
+    } else {
+        showConfirmPopup();
+    }
+}
+
+
+
+
+
+
+
+function showConfirmModal() {
+    $("#dialog-confirm").dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+          "Yes!": function() {
+            alert("Great answer and thanks so much! I can't wait to begin!!! :)");
+            $(this).dialog("close");
+          },
+          "No": function() {
+            alert("Try again :)");
+          }
+        },
+        closeOnEscape: false,
+        open: function(event, ui) {
+            $(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
+        }
+      });
+    $("#dialog-confirm").show();
+}
